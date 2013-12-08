68aa131118f6abce4d5551ee24a4452417021ed1
Nathaniel Pawelczyk <npp40k@yahoo.com>
2013-12-08 15:29:17 -0500

    changed char to X

--- nmap/nmap.cc
+++ nmap/nmap.cc
@@ -2507,9 +2507,9 @@ static void getpts_aux(const char *origexpr, int nested, u8 *porttbl, int range_
                 range_type = SCAN_PROTOCOLS;
                 continue;
             }
-            if (*current_range == '!' && *++current_range == ':') {
+            if (*current_range == 'X' && *++current_range == ':') {
                 current_range++;
-                printf("found the !\n");
+                printf("found the X\n");
                 exclude = 1;
                 continue;
             }
